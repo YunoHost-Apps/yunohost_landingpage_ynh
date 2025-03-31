adcad78e735f9350914a7d9c00015f1ecbd0ad7f
Félix Piédallu <felix.piedallu@non.se.com>
2025-03-31 14:21:12 +0200

    Add the update.sh script

---
+++ conf/update.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+INSTALL_DIR=__INSTALL_DIR__
+
+# There are always some translation related changes
+git -C "$INSTALL_DIR/landingpage" stash
+
+# Update the repo...
+git -C "$INSTALL_DIR/landingpage" pull
+
+# Update the web page!
+LANDINGPAGE_DIST_DIR="$INSTALL_DIR/www_next" bash -x "$INSTALL_DIR/landingpage/bin/regenerate.sh"
+
+# "atomic" replace
+rm -rf "$INSTALL_DIR/www"
+mv "$INSTALL_DIR/www_next" "$INSTALL_DIR/www"
