db11ed754132d957c85323992d24691cea2bd13d
Félix Piédallu <felix@piedallu.me>
2026-07-05 20:13:24 +0200

    nginx: Rename lang_map.conf to prevent yunohost regen-conf to see it

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -4,4 +4,4 @@
 # COMMON VARIABLES AND CUSTOM HELPERS
 #=================================================
 
-lang_map_conf="/etc/nginx/conf.d/${domain}_${app}_lang_map.conf"
+lang_map_conf="/etc/nginx/conf.d/${domain/./_}_${app}_lang_map.conf"
